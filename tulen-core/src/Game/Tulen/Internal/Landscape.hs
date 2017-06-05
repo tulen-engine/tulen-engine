755148df5692d419800bd91198b67fcbcf0ddb8f
Anton Gushcha <ncrashed@gmail.com>
2017-06-05 09:26:10 +0300

    Add landscape mesh module

--- tulen-core/src/Game/Tulen/Internal/Landscape.hs
+++ tulen-core/src/Game/Tulen/Internal/Landscape.hs
@@ -3,4 +3,5 @@ module Game.Tulen.Internal.Landscape(
     module X
   ) where
 
+import Game.Tulen.Internal.Landscape.Mesh as X
 import Game.Tulen.Internal.Landscape.Types as X
---
+++ tulen-core/src/Game/Tulen/Internal/Landscape/Mesh.hs
@@ -0,0 +1,4 @@
+-- | Mesh generation for landscape
+module Game.Tulen.Internal.Landscape.Mesh where
+
+import Game.Tulen.Internal.Landscape.Types
